383c02062fcda0ef8f1e3f7c8387497e59c5a431
ansuz <ansuz@transitiontech.ca>
2016-06-06 17:26:45 +0200

    fix some bugs in the example app

--- www/template/main.js
+++ www/template/main.js
@@ -1,40 +1,47 @@
 define([
     '/bower_components/chainpad-listmap/chainpad-listmap.js',
-    '/bower_components/chainpad-crypto/crypto.js'
+    '/bower_components/chainpad-crypto/crypto.js',
+    '/bower_components/jquery/dist/jquery.min.js',
 ], function (Listmap, Crypto) {
+    var $ = window.jQuery;
 
-    var userName = window.prompt('What is your name?');
+    var userName = window.prompt("What is your name?");
 
-    var config = {
-        "websocketURL":"ws://localhost:3001/cryptpad_websocket",
+    var rt = Listmap.create({
+        "websocketURL":"ws://" + window.location.host + "/cryptpad_websocket",
         "channel":"b87dff2e9a465f0e0ae36453d19b087c",
         "cryptKey":"sksyaHv+OOlQumRmZrQU4f5N",
         "data":{},
         crypto: Crypto
+    });
+
+    var $guestbook = $('#visitors');
+
+    var render = function (visitors) {
+        $guestbook.text(function () {
+            return visitors
+                .filter(function (x) { return x; })
+                .map(function (name) {
+                    return '* ' + name;
+                }).join('\n');
+        });
     };
 
-    var rt = Listmap.create(config);
-
-    var proxy = rt.proxy;
-
-    proxy.on('create', function (info) {
-
-    }).on('ready', function (info) {
+    var proxy = rt.proxy.on('ready', function (info) {
         console.log("Ready!");
-        console.log(JSON.stringify(proxy));
 
         proxy.on('change', ['guestBook'], function (o, n, p) {
-            console.log(JSON.stringify(proxy));
+            render(proxy.guestBook);
         }).on('disconnect', function (info) {
             window.alert("Network Connection Lost!");
         });
 
-        if (!proxy.guestBook) {
-            proxy.guestBook = [];
-        }
+        if (!proxy.guestBook) { proxy.guestBook = []; }
 
         if (proxy.guestBook.indexOf(userName) === -1) {
             proxy.guestBook.push(userName);
         }
+
+        render(proxy.guestBook);
     });
 });
