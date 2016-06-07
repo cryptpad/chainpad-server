a265d41ef72afbe38cdb19fc1fa463c83d23ffba
ansuz <ansuz@transitiontech.ca>
2016-06-07 17:16:28 +0200

    template : load jquery, alert hello world

--- www/template/main.js
+++ www/template/main.js
@@ -1,5 +1,8 @@
 define([
     // dependencies go here
+    '/bower_components/jquery/dist/jquery.min.js'
 ], function (/* modules go here */) {
     // application logic goes here
+
+    window.alert("Hello world!");
 });
